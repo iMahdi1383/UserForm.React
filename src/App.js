aa4486542c95273f7b77f1b8729f1fc453aab61a
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-08-12 17:06:35 +0430

    Add form (how to update ubject states)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [user, setUser] = useState({
+    firstname: 'Mahdi',
+    lastname: 'Askarpoor',
+  });
+
+  const FirstnameChanged = (e) => {
+    /*
+    ?  if we do not use '...user' here, firstname will be changed successfully
+    !  but lastname will be delete.
+    */
+    setUser({ ...user, firstname: e.target.value });
+  };
+
+  const LastnameChanged = (e) => {
+    /*
+    ?  if we do not use '...user' here, lastname will be changed successfully
+    !  but firstname will be delete.
+    */
+    setUser({ ...user, lastname: e.target.value });
+  };
+
   return (
-    <div>
+    <form>
       <h1>User Form</h1>
       <hr />
-
-    </div>
+      <label htmlFor="firstname">FirstName: </label>
+      <input
+        onChange={FirstnameChanged}
+        id="firstname"
+        name="firstname"
+        type="text"
+      />
+      <br />
+      <label htmlFor="lastname">LastName: </label>
+      <input
+        onChange={LastnameChanged}
+        id="lastname"
+        name="lastname"
+        type="text"
+      />
+      <hr />
+      <h3>
+        I am {user.firstname} {user.lastname}.
+      </h3>
+      <code
+        style={{
+          backgroundColor: '#2d2a2e',
+          color: '#fafafa',
+          padding: '0.5rem',
+        }}
+      >
+        {JSON.stringify(user)}
+      </code>
+    </form>
   );
 };
 
